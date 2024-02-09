8f5486615b779a37fc07d70ff34795c1350a4c45
MarcosMiotto711 <marcos.am3@puccampinas.edu.br>
2024-02-09 18:01:58 -0300

    adição do processamento multinuclear para o programa

--- main.py
+++ main.py
@@ -1,43 +1,44 @@
 from customtkinter import *
 from tkinter import filedialog
+from threading import Thread
 import pytube
 import os
 
 # Função que executa o download das musicas
 def musicDownload():
     destination = selection_button.cget('text')
-    form = formato.get()
+    extension = extension_menu.get()
     if destination == 'Pendrive':
         return
     if URL.get() == '':
         return
     if URL.get().find('list') == -1:
         yt = pytube.YouTube(URL.get())
-        if form == 'mp4':
+        if extension == 'mp4':
             yt.streams.filter(type='video', file_extension='mp4', progressive=True).get_highest_resolution().download(output_path=destination)
         else:
-            out_file = yt.streams.filter(only_audio=True, file_extension='mp4', progressive=False).get_audio_only().download(output_path=destination)
+            out_file = yt.streams.filter(file_extension='mp4', progressive=False).get_audio_only().download(output_path=destination)
             base, ext = os.path.splitext(out_file) 
-            new_file = base + f'.{form}'
+            new_file = base + f'.{extension}'
             os.rename(out_file, new_file)
     else:
         p = pytube.Playlist(URL.get())
-        if not p:
-            print('Playlist não existe ou modo de exibição é particular, tente mudar para não listada ou pública!')
+        destination = destination + f'/{p.title}'
+        if extension == 'mp4':
+            for video in p.videos:
+                print(video.title)
+                video.streams.filter(type='video', file_extension='mp4', progressive=True).get_lowest_resolution().download(output_path=destination)
         else:
-            print('Download')
+            for video in p.videos:
+                print(video.title)
+                out_file = video.streams.filter(only_audio=True, file_extension='mp4', progressive=False).get_audio_only().download(output_path=destination)
+                base, ext = os.path.splitext(out_file) 
+                new_file = base + f'.{extension}'
+                os.rename(out_file, new_file)
 
-        # if form == 'mp4':
-        #     for video in p.videos:
-        #         print(video.title)
-        #         video.streams.filter(type='video', file_extension='mp4', progressive=True).get_highest_resolution().download(output_path=destination)
-        # else:
-        #     for video in p.videos:
-        #         print(video.title)
-        #         out_file = video.streams.filter(only_audio=True, file_extension='mp4', progressive=False).get_audio_only().download(output_path=destination)
-        #         base, ext = os.path.splitext(out_file) 
-        #         new_file = base + f'.{form}'
-        #         os.rename(out_file, new_file)
+def execDownload():
+    thread = Thread(target=musicDownload)
+    thread.start()
 
 # Função para selecionar o pendrive
 def flashSelection():
@@ -58,15 +59,24 @@ app.title('Purple Flash')
 
 # Widgets
 URL = CTkEntry(master=app, placeholder_text='Link da playlist, vídeo ou música', border_color='#7b2cbf', 
-                placeholder_text_color='#7b2cbf', font=('Roboto', 18), width=860)
+                placeholder_text_color='#7b2cbf', font=('Roboto', 18), corner_radius=12, width=860)
 URL.place(x=170, y=350)
-formato = CTkOptionMenu(master=app, font=('Roboto', 20), dropdown_font=('Roboto', 20), fg_color='#7b2cbf',
+message_box = CTkLabel(master=app, text='espaço reservado', font=('Roboto', 16), text_color='#7b2cbf', anchor='center',
+                width=860, height=20)
+message_box.place(x=170, y=385)
+type_menu = CTkOptionMenu(master=app, font=('Roboto', 20), dropdown_font=('Roboto', 20), fg_color='#7b2cbf',
                         button_color='#7b2cbf', button_hover_color='#5a189a', dropdown_hover_color='#7b2cbf',
-                        width=90, values=['mp4', 'mp3', 'wav'])
-formato.place(x=1010, y=400)
-selection_button = CTkButton(master=app, text='Pendrive', font=('Roboto', 20), width=170, command=flashSelection)
-selection_button.place(x=200, y=550)
-download_button = CTkButton(master=app, text='Download', font=('Roboto', 20), width=170, command=musicDownload)
-download_button.place(x=200, y=450)
+                        corner_radius=10, width=90, values=['Vídeo', 'Áudio'])
+type_menu.place(x=300, y=410)
+extension_menu = CTkOptionMenu(master=app, font=('Roboto', 20), dropdown_font=('Roboto', 20), fg_color='#7b2cbf',
+                        button_color='#7b2cbf', button_hover_color='#5a189a', dropdown_hover_color='#7b2cbf',
+                        corner_radius=10, width=90, values=['mp4', 'mp3', 'wav'])
+extension_menu.place(x=400, y=410)
+selection_button = CTkButton(master=app, text='Pendrive', fg_color='#7b2cbf', hover_color='#5a189a', 
+                        font=('Roboto', 20), corner_radius=12, width=170, command=flashSelection)
+selection_button.place(x=500, y=410)
+download_button = CTkButton(master=app, text='Download', fg_color='#7b2cbf', font=('Roboto', 20), 
+                        hover_color='#5a189a', corner_radius=12, width=170, command=execDownload)
+download_button.place(x=690, y=410)
 
 app.mainloop()
